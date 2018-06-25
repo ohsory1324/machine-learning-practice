9655d7f00930bc83aa66cbb48ba78a76669edcbc
Yungoo Kong <ohsory1324@gmail.com>
2018-06-25 14:24:34 +0900

    add: learn perceptron

--- perceptron.js
+++ perceptron.js
@@ -25,7 +25,52 @@ const XOR = (x1, x2) => {
   return AND(s1, s2);
 };
 
+const sigmoid = x => 1 / (1 + Math.exp(-x));
+const activation = x => x >= 0.5 ? 1 : 0;
+
+const learn = (logic, steps = 1000, n = 0.01) => {
+  const w = [Math.random() * 2 - 1, Math.random() * 2 - 1];
+  let error = 0;
+  let yhList = [];
+  let b = 1;
+  let count = 0;
+
+  const inputs = [[0, 0], [0, 1], [1, 0], [1, 1]];
+  const y = inputs.map(([x1, x2]) => logic(x1, x2));
+  while (count < steps) {
+    inputs.forEach(([x1, x2], i) => {
+      const [w1, w2] = w;
+      const net = w1 * x1 + w2 * x2 + b;
+      const yh = activation(net);
+
+      w[0] = w1 + n * (x1 * (y[i] - yh));
+      w[1] = w2 + n * (x2 * (y[i] - yh));
+      b += n * (y[i] - yh);
+      error += Math.abs(yh - y[i]);
+      yhList.push(yh);
+    });
+    count += 1;
+    console.log('step: ', count);
+    console.log('실제값: ', y);
+    console.log('예측값: ', yhList);
+    console.log('w: ', w);
+    console.log('b: ', b, '\n');
+
+    if (count === steps) {
+      throw new Error('Not solved!');
+    } else if (error === 0) {
+      break;
+    } else {
+      yhList = [];
+      error = 0;
+    }
+  }
+
+  return { w, b };
+};
+
 module.exports = {
+  learn,
   AND,
   OR,
   NAND,
--- perceptron.test.js
+++ perceptron.test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 
-const { AND, OR, NAND, XOR } = require('./perceptron');
+const { learn, AND, OR, NAND, XOR } = require('./perceptron');
 
 describe('AND', () => {
   it('should return 1 when x1 === 1 and x2 === 1', () => {
@@ -59,5 +59,23 @@ describe('XOR', () => {
   });
   it('should return 1 when x1 === 0 and x2 === 1', () => {
     expect(XOR(0, 1)).to.equal(1);
+  });
+});
+
+describe('learn perceptron', () => {
+  it('should solve AND logic with perceptron', () => {
+    expect(learn(AND, 1000)).not.to.be.an('error');
+  });
+
+  it('should solve OR logic with perceptron', () => {
+    expect(learn(OR, 1000)).not.to.be.an('error');
+  });
+
+  it('should solve NAND logic with perceptron', () => {
+    expect(learn(NAND, 1000)).not.to.be.an('error');
+  });
+
+  it('should NOT solve XOR logic with perceptron', () => {
+    expect(learn.bind(learn, XOR, 1000)).to.throw();
   });
 });
