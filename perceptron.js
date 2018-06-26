ec2d682098cc5bd746744c86241f0b88dc8cac1d
Yungoo Kong <ohsory1324@gmail.com>
2018-06-26 17:36:43 +0900

    fix: perceptron

--- perceptron.js
+++ perceptron.js
@@ -25,14 +25,22 @@ const XOR = (x1, x2) => {
   return AND(s1, s2);
 };
 
-const sigmoid = x => 1 / (1 + Math.exp(-x));
-const activation = x => x >= 0.5 ? 1 : 0;
+const step = x => x >= 0.5 ? 1 : 0;
+const sigmoid = x => 1 / (1 + Math.exp(-x));;
 
-const learn = (logic, steps = 1000, n = 0.01) => {
-  const w = [Math.random() * 2 - 1, Math.random() * 2 - 1];
+const learn = ({
+  logic = AND,
+  steps = 1000,
+  n = 0.01,
+  activation = step,
+  log = false,
+  init,
+}) => {
+  const { w: wInit, b: bInit } = init || {};
+  const w = wInit || [Math.random() * 2 - 1, Math.random() * 2 - 1];
+  let b = bInit || 1;
   let error = 0;
   let yhList = [];
-  let b = 1;
   let count = 0;
 
   const inputs = [[0, 0], [0, 1], [1, 0], [1, 1]];
@@ -40,25 +48,25 @@ const learn = (logic, steps = 1000, n = 0.01) => {
   while (count < steps) {
     inputs.forEach(([x1, x2], i) => {
       const [w1, w2] = w;
-      const net = w1 * x1 + w2 * x2 + b;
-      const yh = activation(net);
+      const yh = activation(w1 * x1 + w2 * x2 + b);
+      const correction = n * (y[i] - yh);
 
-      w[0] = w1 + n * (x1 * (y[i] - yh));
-      w[1] = w2 + n * (x2 * (y[i] - yh));
-      b += n * (y[i] - yh);
+      w[0] = w1 + x1 * correction;
+      w[1] = w2 + x2 * correction;
+      b += correction;
       error += Math.abs(yh - y[i]);
       yhList.push(yh);
     });
     count += 1;
-    console.log('step: ', count);
-    console.log('실제값: ', y);
-    console.log('예측값: ', yhList);
-    console.log('w: ', w);
-    console.log('b: ', b, '\n');
+    if (log) {
+      console.log('step: ', count);
+      console.log('실제값: ', y);
+      console.log('예측값: ', yhList);
+      console.log('w: ', w);
+      console.log('b: ', b, '\n');
+    }
 
-    if (count === steps) {
-      throw new Error('Not solved!');
-    } else if (error === 0) {
+    if (count === steps || error === 0) {
       break;
     } else {
       yhList = [];
@@ -75,4 +83,8 @@ module.exports = {
   OR,
   NAND,
   XOR,
+  activation: {
+    step,
+    sigmoid,
+  }
 };
--- perceptron.test.js
+++ perceptron.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 
-const { learn, AND, OR, NAND, XOR } = require('./perceptron');
+const { learn, AND, OR, NAND, XOR, activation } = require('./perceptron');
 
 describe('AND', () => {
   it('should return 1 when x1 === 1 and x2 === 1', () => {
@@ -63,19 +63,92 @@ describe('XOR', () => {
 });
 
 describe('learn perceptron', () => {
-  it('should solve AND logic with perceptron', () => {
-    expect(learn(AND, 1000)).not.to.be.an('error');
+  const inputs = [[0, 0], [0, 1], [1, 0], [1, 1]];
+  const net = (w, b, x) => w[0] * x[0] + w[1] * x[1] + b;
+
+  describe('step activation', () => {
+    const defaultOptions = {
+      logic: AND,
+      activation: activation.step,
+      log: false,
+      n: 0.1,
+      steps: 1000,
+    };
+
+    it('should solve AND logic with perceptron', () => {
+      const { w, b } = learn(defaultOptions);
+      inputs.forEach((input) => {
+        const x = net(w, b, input);
+        const y = activation.step(x);
+        const yh = AND(...input);
+        expect(Math.abs(y - yh)).to.lessThan(0.01);
+      });
+    });
+  
+    it('should solve OR logic with perceptron', () => {
+      const { w, b } = learn({ ...defaultOptions, logic: OR });
+      inputs.forEach((input) => {
+        const x = net(w, b, input);
+        const y = activation.step(x);
+        const yh = OR(...input);
+        expect(Math.abs(y - yh)).to.lessThan(0.01);
+      });
+    });
+  
+    it('should solve NAND logic with perceptron', () => {
+      const { w, b } = learn({ ...defaultOptions, logic: NAND });
+      inputs.forEach((input) => {
+        const x = net(w, b, input);
+        const y = activation.step(x);
+        const yh = NAND(...input);
+        expect(Math.abs(y - yh)).to.lessThan(0.01);
+      });
+    });
+  
+    // it('should NOT solve XOR logic with perceptron', () => {
+    // });
   });
 
-  it('should solve OR logic with perceptron', () => {
-    expect(learn(OR, 1000)).not.to.be.an('error');
-  });
+  describe('sigmoid activation', () => {
+    const defaultOptions = {
+      logic: AND,
+      activation: activation.sigmoid,
+      log: false,
+      n: 0.1,
+      steps: 10000,
+    };
 
-  it('should solve NAND logic with perceptron', () => {
-    expect(learn(NAND, 1000)).not.to.be.an('error');
-  });
-
-  it('should NOT solve XOR logic with perceptron', () => {
-    expect(learn.bind(learn, XOR, 1000)).to.throw();
+    it('should solve AND logic with perceptron', () => {
+      const { w, b } = learn({ ...defaultOptions });
+      inputs.forEach((input) => {
+        const x = net(w, b, input);
+        const y = activation.sigmoid(x);
+        const yh = AND(...input);
+        expect(Math.abs(y - yh)).to.lessThan(0.01);
+      });
+    });
+  
+    it('should solve OR logic with perceptron', () => {
+      const { w, b } = learn({ ...defaultOptions, logic: OR });
+      inputs.forEach((input) => {
+        const x = net(w, b, input);
+        const y = activation.sigmoid(x);
+        const yh = OR(...input);
+        expect(Math.abs(y - yh)).to.lessThan(0.01);
+      });
+    });
+  
+    it('should solve NAND logic with perceptron', () => {
+      const { w, b } = learn({ ...defaultOptions, logic: NAND });
+      inputs.forEach((input) => {
+        const x = net(w, b, input);
+        const y = activation.sigmoid(x);
+        const yh = NAND(...input);
+        expect(Math.abs(y - yh)).to.lessThan(0.01);
+      });
+    });
+  
+    // it('should NOT solve XOR logic with perceptron', () => {
+    // });
   });
 });
